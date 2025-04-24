bc1143d4d4f830404e8a1dbb656f41dd76fdc81b
Albert826-js <albert19521990@gmail.com>
2025-04-24 21:51:05 +0530

    Add files via upload

---
+++ map.js
@@ -0,0 +1,76 @@
+let a =new Map();
+
+let b={
+    name:()=>{
+        console.log("hi");
+    }
+}
+
+console.log(b.name());
+
+a.set('name',()=>{
+    console.log("how");
+});
+a.set(1,"hi");
+
+a.set('car',{data:["albert","steve"]});
+
+
+
+
+
+let cardetails=a.get('car'.s)
+console.log(cardetails);
+
+
+
+
+let y=a.get('name');
+
+console.log(y());
+
+
+let value=["alvert",3,"fhfj"];
+console.log(value.splice(1,0,"rek"));
+console.log(value);
+
+
+function na() {
+    this.gram=[];
+    this.dater =new Array("albert","rend");
+    this.obj=["time"];
+     
+    console.log(dater.length + obj);
+
+  
+for (const i of value) {
+    console.log(i);
+    
+    
+}
+    
+    console.log(gram.push(dater));
+    console.log(gram[0]);
+    console.log(gram);
+    
+    
+    
+
+    
+        
+}
+
+
+
+na();
+
+
+
+let last=["ji"];
+let brat =new Map();
+brat.set("albert","dustin");
+console.log(brat.get('albert'));
+
+
+ 
+
