26f87ae99dd46dabc4b4ffc8a21566730b958871
Albert826-js <albert19521990@gmail.com>
2025-04-24 21:50:32 +0530

    Add files via upload

---
+++ map2.js
@@ -0,0 +1,48 @@
+let arr=["time","fine","wine"];
+console.log(arr.push("retro"));
+
+
+
+let map =arr.map((mapper)=>{
+    console.log(mapper.toUpperCase(mapper));
+    console.log(mapper.endsWith("e"));
+    
+
+
+    
+    
+});
+
+
+
+let bonus=[1,2]
+
+function bo() {
+     console.log(bonus);
+     
+     
+    
+    
+};
+
+let orac={
+    name:"albert",
+    fun:()=>{
+        console.log("gethhhh");
+        
+    }
+
+}
+
+let mapp=bonus.map((bot)=>{
+    console.log(orac);
+    
+    
+
+});
+ 
+
+ 
+
+
+
